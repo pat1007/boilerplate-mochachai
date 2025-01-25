a617d7fa990101f50b3f5be5224f4c7859ab77ac
pat1007 <143038040+pat1007@users.noreply.github.com>
2025-01-24 19:19:44 -0500

    Update 1_unit-tests.js

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -3,7 +3,7 @@ const assert = chai.assert;
 
 suite('Unit Tests', function () {
   suite('Basic Assertions', function () {
-    // #1
+    // #1 assert.isNull() {
     test('#isNull, #isNotNull', function () {
       assert.fail(null, 'This is an optional error description - e.g. null is null');
       assert.fail(1, '1 is not null');
